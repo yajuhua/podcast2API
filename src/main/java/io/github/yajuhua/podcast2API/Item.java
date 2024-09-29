935f5179805719a3c596d7fed62e15ad37d94811
yajuhua <yajuhua@outlook.com>
2024-09-29 20:11:21 +0800

    [feat]新增publicTime字段用于存放原视频发布时间

--- src/main/java/io/github/yajuhua/podcast2API/Item.java
+++ src/main/java/io/github/yajuhua/podcast2API/Item.java
@@ -26,6 +26,10 @@ public class Item {
     private boolean escapeImage = true;//默认转义
     private Request request;//下载请求
     /**
+     * 原视频发布时间
+     */
+    private Long publicTime;
+    /**
      * 附件类型 如video/mp4
      */
     private String enclosureType;
